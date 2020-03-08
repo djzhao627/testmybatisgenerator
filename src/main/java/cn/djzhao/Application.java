0f9140270ae9ada892b07899f40b20266ff59967
djzhao <djzhao627@gmail.com>
2020-03-08 15:01:39 +0800

    Initial commit

---
+++ src/main/java/cn/djzhao/Application.java
@@ -0,0 +1,16 @@
+package cn.djzhao;
+
+/**
+ * 启动器
+ *
+ * @author djzhao
+ * @date 19/10/21 16:31
+ * @email djzhao627@gmail.com
+ */
+public class Application {
+    public static void main(String[] args) {
+        String a = "aslkdjlasj";
+        System.out.println(a);
+        System.out.println("every this is awesome");
+    }
+}
